878b28a3295253e6e48f0468c66f78709ff303bd
Arijeet10 <arijeetsocial@gmail.com>
2024-02-29 10:11:34 +0530

    metaverus landing page using framer motion

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Launch of the Metaverse makes Elon Musk wonder',
+    subtitle:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur exercitationem ratione vel consequuntur illum id voluptate, dolor debitis. Fugit tempore consectetur maiores accusamus eum aperiam, omnis eaque necessitatibus sint adipisci?',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur exercitationem ratione vel consequuntur illum id voluptate, dolor debitis. Fugit tempore consectetur maiores accusamus eum aperiam, omnis eaque necessitatibus sint adipisci?',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Explore the whole world virtually',
+    subtitle:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur exercitationem ratione vel consequuntur illum id voluptate, dolor debitis. Fugit tempore consectetur maiores accusamus eum aperiam, omnis eaque necessitatibus sint adipisci?',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
